66b5486a27b4c636467abec327c8bfd20908d869
nickantonaccio <nick@guitarz.org>
2022-04-04 14:09:03 -0500

    Update streamlit_app.py

--- blob/master/streamlit_app.py
+++ blob/master/streamlit_app.py
@@ -6,3 +6,15 @@ st.set_page_config(
 
 
 number = st.slider("Pick a number", 0, 100)
+
+d = st.date_input(
+     "When's your birthday",
+     datetime.date(2019, 7, 6))
+st.write('Your birthday is:', d)
+
+import streamlit as st
+
+picture = st.camera_input("Take a picture")
+
+if picture:
+     st.image(picture)
