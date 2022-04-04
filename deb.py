3a3ab96ec45da67dcb44c42a866dd024b3b005a0
nickantonaccio <nick@guitarz.org>
2022-04-04 18:30:48 -0500

    Update deb.py

--- deb.py
+++ deb.py
@@ -1,13 +1,15 @@
 import streamlit as st
 
 howmuch = st.slider('How much do you dig Nick?', 0, 100)
+if howmuch:
+    st.write('You dig nick: ', howmuch)
 
 d = st.date_input(
     "Please pick tomorrow's date",
 )
+if d:
+    st.write('Deb will still be totally awesome on: ', d)
 
 picture = st.camera_input("Take a picture")
 if picture:
     st.image(picture)
-    st.write('Deb will still be totally awesome on: ', d)
-    st.write('You dig nick: ', howmuch)
