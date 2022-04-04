6ecbb218c3eae328691224c420bf051d6c3d140d
nickantonaccio <nick@guitarz.org>
2022-04-04 13:58:02 -0500

    Update streamlit_app.py

--- blob/master/streamlit_app.py
+++ blob/master/streamlit_app.py
@@ -1,1 +1,11 @@
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+df_wc=df[(df['tags'] =='education')]
+df_wc=df_wc[['talk_name','views']]
+df_wc = df_wc.set_index('talk_name').to_dict()['views']
 
+wc = WordCloud(width=800, height=400, max_words=200,).generate_from_frequencies(df_wc)
+plt.figure(figsize=(10, 10))
+plt.imshow(wc, interpolation='bilinear')
+plt.axis('off')
+plt.show()
