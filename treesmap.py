17d5f906b7ace683d60e28d8a70352941de6c187
nickantonaccio <nick@guitarz.org>
2022-04-13 23:53:01 -0500

    Update treesmap.py

--- treesmap.py
+++ treesmap.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 st.title('SF Trees')
 st.write('This app analyses trees in San Francisco using'
-	 ' a dataset kindly provided by SF DPW')
+	 ' the following dataset:')
 trees_df = pd.read_csv('trees.csv')
 trees_df = trees_df.dropna(subset=['longitude', 'latitude'])
 trees_df = trees_df.sample(n = 1000)
