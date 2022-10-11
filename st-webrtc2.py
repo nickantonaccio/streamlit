f13603f88d19db362420df0365fa275908524c64
nickantonaccio <nick@guitarz.org>
2022-10-11 17:47:53 -0500

    Add files via upload

---
+++ st-webrtc2.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer
+import av
+import cv2
+
+st.title("My first Streamlit app")
+st.write("Hello, world")
+
+
+def callback(frame):
+    img = frame.to_ndarray(format="bgr24")
+
+    img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
+
+    return av.VideoFrame.from_ndarray(img, format="bgr24")
+
+
+webrtc_streamer(key="example", video_frame_callback=callback)
