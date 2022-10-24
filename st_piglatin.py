e7dcab6e09c0a639223fb2d0334994ec758fb009
nickantonaccio <nick@guitarz.org>
2022-10-24 13:52:23 -0400

    Update st_piglatin.py

--- st_piglatin.py
+++ st_piglatin.py
@@ -1,1 +1,11 @@
-st.write('piglatin')
+text = st.text_area('Type or paste some  text')
+if text:
+  word_list = text.split(' ')
+  pig_latin = ' '
+  for word in word_list:
+    if word.isalpha():
+      pigword = word[1:] + word[0] + 'ay'
+      pig_latin = pig_latin + pigword + ' '
+    else:
+      pig_latin += word
+  st.write(pig_latin.strip(' '))
