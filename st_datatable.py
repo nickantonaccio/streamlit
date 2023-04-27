3cb318b201cd97dd0143c0b952edb3f947d89f7a
nickantonaccio <nick@guitarz.org>
2023-04-27 10:09:36 -0400

    Update st_datatable.py

--- st_datatable.py
+++ st_datatable.py
@@ -1,1 +1,12 @@
-st.write('datatable')
+import streamlit as st, dataset
+db=dataset.connect('sqlite:///db'); t=db['user']     #t=table
+if st.button('Add Row'): t.insert(dict(x='',y=''))   #x,y=name, note
+for r in t:                                          #r=row in table
+  with st.expander(r['x']):
+    with st.form(str(r['id'])):
+      x=st.text_input('Name', r['x'])
+      y=st.text_area('Note', r['y'])
+      if st.form_submit_button('Save'):
+        t.update(dict(x=x,y=y,id=r['id']),['id'])
+      if st.form_submit_button('Delete'):
+        t.delete(id=r['id']); st.experimental_rerun()
